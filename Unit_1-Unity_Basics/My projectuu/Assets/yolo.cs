dd41b48e5b6f0bd9b3b98ffe46ec679f09420c68
Sandra Cruz <75856359+scruztaehs@users.noreply.github.com>
2024-11-13 15:11:14 -0500

    sf

---
+++ Unit_1-Unity_Basics/My projectuu/Assets/yolo.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yolo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        xhfxhfxhxkhvkhjvfgr
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
